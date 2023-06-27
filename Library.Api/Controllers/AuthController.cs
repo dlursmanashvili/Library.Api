69dc2d47a69c719b4bcee5b5a323ecd9180e80d5
d.lursmanashvili <datolursmanishvili64@gmail.com>
2023-06-27 18:59:48 +0400

    add code

--- Library.Api/Controllers/AuthController.cs
+++ Library.Api/Controllers/AuthController.cs
@@ -1,12 +1,8 @@
-﻿using Library.Models.Employee;
+﻿using Library.Infrastructure.HelperClass;
+using Library.Models.Employee;
 using Library.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace Library.Controller;
 
@@ -16,7 +12,6 @@ public class AuthController : ControllerBase
 {
     private readonly EmployeeService _employeeService;
     public IConfiguration _icnfiguration { get; }
-
     public AuthController(IConfiguration icnfiguration)
     {
         _icnfiguration = icnfiguration;
@@ -27,7 +22,7 @@ public class AuthController : ControllerBase
     {
         try
         {
-            CreatePasswordHash(employee.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            SecurityHelper.CreatePasswordHash(employee.Password, out byte[] passwordHash, out byte[] passwordSalt);
             var NewUser = new Employee()
             {
                 Id = new Guid(),
@@ -43,8 +38,8 @@ public class AuthController : ControllerBase
         {
             throw ex;
         }
-
     }
+
     [HttpPost("login")]
     public async Task<ActionResult<string>> Login(EmployeeDto request)
     {
@@ -53,45 +48,11 @@ public class AuthController : ControllerBase
         {
             return BadRequest("user Not found");
         }
-        if (!VerifyPasswordHash(request.Password, employee.PasswordHash, employee.PasswordSalt))
+        if (!SecurityHelper.VerifyPasswordHash(request.Password, employee.PasswordHash, employee.PasswordSalt))
         {
             return BadRequest("Password not correct");
         }
-        return Ok("successful");
+        return Ok(SecurityHelper.CreateToken(employee));
     }
 
-    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-    {
-        using (var hmac = new HMACSHA512(passwordSalt))
-        {
-            var computerHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-            return computerHash.Equals(passwordHash);
-        }
-
-    }
-    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-    {
-        using (var hmac = new HMACSHA512())
-        {
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-        }
-    }
-
-    private string CreateToken(Employee user)
-    {
-        List<Claim> claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name,user.Email)
-        };
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_icnfiguration.GetSection("AppSetting:Token").Value));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-        var token = new JwtSecurityToken(
-            claims: claims,
-            expires: DateTime.Now.AddDays(1),
-            signingCredentials: creds);
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
-    }
 }
