2a301696147070768f20b93928b0342ee3f63aba
d.lursmanashvili <datolursmanishvili64@gmail.com>
2023-07-02 17:27:38 +0400

    add OAuth

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,14 +1,11 @@
-using Library.Api;
+﻿using Library.Api;
 using Library.Api.Middlewares;
 using Library.Infrastructure.DataBaseHelper;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json;
 using System.Net.Http.Headers;
 using System.Security.Claims;
-using Newtonsoft.Json.Linq;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,70 +23,48 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
                                                                               .AllowAnyMethod()
                                                                               .AllowAnyHeader()));
-
-
-builder.Services.AddAuthentication(options =>
-{
-    // If an authentication cookie is present, use it to get authentication information
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-
-    // If authentication is required, and no cookie is present, use Okta (configured below) to sign in
-    options.DefaultChallengeScheme = "Okta";
-})
-.AddCookie() // cookie authentication middleware first
-.AddOAuth("Okta", options =>
-{
-    // Oauth authentication middleware is second
-    var oktaDomain = builder.Configuration.GetValue<string>("Okta:OktaDomain");
-
-    // When a user needs to sign in, they will be redirected to the authorize endpoint
-    options.AuthorizationEndpoint = $"{oktaDomain}/oauth2/default/v1/authorize";
-
-    // Okta's OAuth server is OpenID compliant, so request the standard openid
-    // scopes when redirecting to the authorization endpoint
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-
-    // After the user signs in, an authorization code will be sent to a callback
-    // in this app. The OAuth middleware will intercept it
-    options.CallbackPath = new PathString("/authorization-code/callback");
-
-    // The OAuth middleware will send the ClientId, ClientSecret, and the
-    // authorization code to the token endpoint, and get an access token in return
-    options.ClientId = builder.Configuration.GetValue<string>("Okta:ClientId");
-    options.ClientSecret = builder.Configuration.GetValue<string>("Okta:ClientSecret");
-    options.TokenEndpoint = $"{oktaDomain}/oauth2/default/v1/token";
-
-    // Below we call the userinfo endpoint to get information about the user
-    options.UserInformationEndpoint = $"{oktaDomain}/oauth2/default/v1/userinfo";
-
-    // Describe how to map the user info we receive to user claims
-    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
-    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "given_name");
-    options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
-
-    options.Events = new OAuthEvents
+//add Oauth Configuration
+// სანამ ამას დაწერ, პირველი ნაბიჯი ისაა, რომ ხსნი გითჰაბს, შედიხარ სეთინგებში, სულ ბოლოში ირჩევ - <developer settings>, 
+// შემდეგ ირჩევ OAuthApps-ს და ქმნი მანდ ახალ აპლიკაციას. Homepage URL -ში ჩაწერე აპლიკაცია რა ორტზეც ეშვება, ეგ მისამართი,
+// მაგალითად - https://localhost:7199;
+// Authorization callback URL - საწყის ეტაპზე https://localhost:7199 - მერე შეცვლი. 
+// 
+//გითჰაპს რომ მორჩები და აპლიკაციას რომ დაარეგისტრირებ, შემდეგ დაიწყე ამ კონფიგურაციის წერა. 
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie")
+    .AddOAuth("github", options =>
     {
-        OnCreatingTicket = async context =>
+        options.SignInScheme = "cookie";
+
+        // ეს ქვედა ორი თუ გინდა ჯეისონში გაიტანე და იქიდან წამოიღე, უკეთესია. თუ გინდა დატოვე.
+        options.ClientId = "0b8893e097d133fe438c";  //ამას წამოიღებ გითჰაბიდან, კლიენტ აიდის აიღებ მანდედან.
+        options.ClientSecret = "9ca337ef462162f72a88451ff36a26f713bfe7bf"; //სექრეთ ქისაც მანდვე დააგენერირებ.
+
+        options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+        options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+        options.SaveTokens = true;
+        options.UserInformationEndpoint = "https://api.github.com/user";
+
+        options.CallbackPath = "/oauth/github-cb"; //აქ შეგიძლია ნებისმიერი რამ ჩაწერო, Path-რაც გინდა რომ გქონდეს, უბრალოდ
+                                                   //როცა ამას დაწერე, იგივე უნდა აიღო და Authorization callback URL - სადაც ჩაწერე გითჰაბზე, იქ მიამატებ -
+                                                   // ანუ Authorization callback URL-ში  https://localhost:7199 ნაცვლად გექნება https://localhost:7199/oauth/github-cb 
+                                                   //ანუ რასაც მანდ დაწერ, იმას მიამატებ. შეგიძლია დატოვო ისე, როგორც არის. 
+
+        //"Sub" stands for "subject"
+        options.ClaimActions.MapJsonKey("sub", "id");
+        options.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+
+        options.Events.OnCreatingTicket = async ctx =>
         {
-            // Get user info from the userinfo endpoint and use it to populate user claims
-            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+            using var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+            using var result = await ctx.Backchannel.SendAsync(request);
+            var user = await result.Content.ReadFromJsonAsync<JsonElement>();
+            ctx.RunClaimActions(user);
+        };
+    });
 
-            var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-            response.EnsureSuccessStatusCode();
-
-            var user  = JObject.Parse(await response.Content.ReadAsStringAsync());
-            var element = JsonSerializer.SerializeToElement(user);
-            
-            context.RunClaimActions(element);
-        }
-    };
-});
-
+//ამას რომ დაწერ, შემდეგ გადადი აპლიკაციაში - properties ფოლდერში, და launchSettings.json გახსენი.
 
 var app = builder.Build();
 
@@ -101,6 +76,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseAuthentication();
 
 app.UseCors("Default");
 
@@ -110,12 +86,41 @@ using (var scope = app.Services.CreateScope())
     dataContext.Database.Migrate();
 }
 
-app.UseHttpsRedirection();
+// ეს პირველი get - მეთოდი აუცილებელი არაა, უბრალოდ იმიტომ დავწერე რომ სადმე გადავემისამართებინე და ქლეიმები გამოეტანა.
+app.MapGet("/auth/homepage", (HttpContext ctx) =>
+{
+    ctx.GetTokenAsync("access_token");
+    return ctx.User.Claims.Select(x => new { x.Type, x.Value }).ToList();
+});
 
-app.UseAuthentication(); 
+//ამ ენდფოინთზე ("/auth/login") კონტროლერი არ კეთდება(ვცადე მარა არ გამოვიდა), რადგან IActionResult-ს არ აბრუნებს, ამიტომ Minimal Api-ით კეთდება, ასე რომ პირდაპირ აქ დაამპლემენტირე.
+// აუცილებლად app.UseAuthentication(); ქვემოთ.
+
+
+app.MapGet("/auth/login", () =>
+{
+    return Results.Challenge(
+    new AuthenticationProperties()
+    {
+        RedirectUri = "https://localhost:7199/auth/homepage"  // აქ შეგიძლია მიუთითო ნებისმიერი url- სადაც გინდა რომ გადამისამართდეს
+    },
+    authenticationSchemes: new List<string>() { "github" });
+
+});
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+// რომ მორჩები, და აპლიკაციას გაუშვებ, ჩაწერე url-ში https://localhost:7199/auth/login და სასწაული მოხდება :D მანამდე გითჰაბიდან დალოგაუთდი
