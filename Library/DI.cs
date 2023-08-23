0a9973d1ddd8dacf20a5eb166fcaf3de8856c417
D.Lursmanashvili <D.Lursmanashvili@USER>
2023-08-23 20:01:08 +0400

    add code

--- Library/DI.cs
+++ Library/DI.cs
@@ -18,5 +18,6 @@ public static class DI
         services.AddScoped<IAuthorRepository, AuthorRepository>();
         services.AddScoped<IBookRepository, BookRepository>();
         services.AddScoped<IBookAuthorRepository, BookAuthorRepository>();
+
     }
 }
